27c8af72257ace690a36be9e6abaadeaa2485a71
Yassine AZZOUT <yassine.azzout@gmail.com>
2019-03-17 23:04:55 +0100

    chore(uik): define starter template and steps

---
+++ App.js
@@ -0,0 +1,42 @@
+import * as React from "react";
+
+import {
+  UikWidgetContent,
+  UikWidget,
+  UikHeadline,
+  UikDivider,
+  UikButton,
+  Uikon
+} from "@uik";
+
+import "@uik/styles.css";
+import "@uik/index.scss";
+import styles from "./app.module.scss";
+
+const BuildingsSignUp = () => (
+  <div className={styles.pageWrapper}>
+    <UikWidget className={styles.widgetWrapper}>
+      <div className={styles.content}>
+        <UikWidgetContent className={styles.left}>
+          <UikHeadline>Ready for UI Kit 3.0</UikHeadline>
+          <UikDivider margin />
+          <h3 className={styles.headline}>
+            <a
+              href="http://preview.janlosert.com/docs/start/project-structure"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kit Documentation
+            </a>
+          </h3>
+          <UikButton className={styles.btnAction} success>
+            Get Started With Screen Previews
+            <Uikon>rocket</Uikon>
+          </UikButton>
+        </UikWidgetContent>
+      </div>
+    </UikWidget>
+  </div>
+);
+
+export default BuildingsSignUp;
---
+++ README.md
@@ -0,0 +1,57 @@
+# UIK
+
+> Bootstrap Dashboard UI Kit 3.0 for my needs
+
+## 🕹️ Usage
+
+```sh
+# cd /path/to/workspace/project
+❯ uik
+```
+
+## 👩‍💻 Just tell me what to do
+
+* Update `.gitignore` file
+
+```
+# UIKit
+src/@uik
+```
+
+* Update `.env` file
+
+```
+NODE_PATH=src
+```
+
+* Create `@uik` dist files to destination directory
+
+```sh
+❯ UIK_PATH=/path/to/uik/path
+❯ PROJECT=/path/to/project
+❯ mkdir -p $PROJECT/src/@uik
+❯ cp $UIK_PATH/dist/lib/* $PROJECT/src/@uik
+❯ cp -r $UIK_PATH/src/universal/fonts $PROJECT/src/@uik/
+❯ cp $UIK_PATH/src/universal/_vars.scss $PROJECT/src/@uik/
+❯ cp $UIK_PATH/src/universal/App/font.scss $PROJECT/src/@uik/
+❯ cp $UIK_PATH/src/universal/App/normalize.css $PROJECT/src/@uik/
+```
+
+* Install required node modules
+
+```sh
+❯ yarn add classnames
+❯ yarn add node-sass
+```
+
+* Copy the starter template
+
+```sh
+❯ cp index.scss $PROJECT/src/@uik/
+❯ cp App.js $PROJECT/src
+❯ cp app.module.scss $PROJECT/src
+```
+
+## 🌱 Inspiration - deep dive reading
+
+* [Dashboard UI Kit 3.0](https://janlosert.com/store/dashboard-ui-kit-3.html)
